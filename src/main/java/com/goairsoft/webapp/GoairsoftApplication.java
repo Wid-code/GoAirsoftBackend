d45eee7d4c11ddec71da8d0509f38a975db04fa7
BlazejW <blazej.widera@onet.pl>
2025-02-10 19:09:11 +0100

    Initial commit

---
+++ src/main/java/com/goairsoft/webapp/GoairsoftApplication.java
@@ -0,0 +1,13 @@
+package com.goairsoft.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoairsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoairsoftApplication.class, args);
+	}
+
+}
